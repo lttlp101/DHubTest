188a127577a568dada626a34dd6b7dd3b8612ad6
Patrick <64219075+lttlp101@users.noreply.github.com>
2025-04-19 00:59:40 -0700

    fix: update PostForm label to make content field required

--- src/components/PostForm/PostForm.jsx
+++ src/components/PostForm/PostForm.jsx
@@ -95,7 +95,7 @@ const PostForm = ({
 			</div>
 
 			<div className={styles.formGroup}>
-				<label htmlFor="content">Content (Optional)</label>
+				<label htmlFor="content">Content</label>
 				<textarea
 					id="content"
 					placeholder={
